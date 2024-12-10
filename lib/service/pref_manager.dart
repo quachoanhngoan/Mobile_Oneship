7f027534bfb5993f84695541f97a24d13e5b1095
Nguyễn Đình Linh <linhngdeveloper@maplelabs.co>
2024-12-11 06:10:25 +0700

    develop brand

--- lib/service/pref_manager.dart
+++ lib/service/pref_manager.dart
@@ -17,7 +17,7 @@ class PrefManager {
 
   /// Light, Dark ,System
 
-  SharedPreferences preferences;
+  late SharedPreferences preferences;
 
   //for Bloc.Bloc.login
   set provinceId(String value) => preferences.setString(kProvinceId, value);
